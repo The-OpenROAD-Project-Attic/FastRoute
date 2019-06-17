5ef46cb6534eb6ae4e35a16873a8d50a0258274b
Vitor Bandeira <vitor.vbandeira@gmail.com>
2019-06-16 17:40:24 -0700

    Add golden reference

--- support/run_tests.sh
+++ support/run_tests.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 bin_path=./fr41/FastRoute
-# bin_path=./best3/FastRoute
-
 log_folder=./logs
+gold_folder=./logs_fr41
+
+# bin_path=./best3/FastRoute
+# log_folder=./logs_best3
+# gold_folder=./logs_best3
+
 data_folder=./best3
 
 input_files=(a1 a2 a3 a5 b1 b2 b3 b4 n1 n2 n3 n4 n5 n6)
@@ -17,3 +21,31 @@ for f in "${input_files[@]}"
 do
         ./FastRoute "$data_folder/$f" -o "$log_folder/$f.output" | grep -vi time &> "$log_folder/$f.log"
 done
+
+failed=0
+suceeded=0
+for f in "${input_files[@]}"
+do
+        out=$(cmp -s "$log_folder/$f" "$gold_folder/$f")
+        if [[ -n "$out" ]]; then
+                echo "Test $f failed"
+                failed=$((failed+1))
+        else
+                echo "Test $f suceeded"
+                suceeded=$((suceeded+1))
+        fi
+done
+
+total=$((failed+suceeded))
+
+cat << HEREDOC
+
+###############################################################################
+#                                 TEST REPORT                                 #
+###############################################################################
+
+Total tests $total
+Failed = $failed ($((100*failed/total))%)
+Suceeded = $suceeded ($((100*suceeded/total))%)
+
+HEREDOC
