3f0841fd782ca393843ec31ac00a58fab3081f67
Eder Matheus Rodrigues Monteiro <emrmonteiro@inf.ufrgs.br>
2019-09-30 19:47:46 -0300

    Update Jenkins Dockerfile and scripts to be compliant with new unit tests

--- jenkins/unit-test.sh
+++ jenkins/unit-test.sh
@@ -1,4 +1,4 @@
 ln -s /fastroute/build/third_party/rsyn/bin/rsyn ./FRlefdef
 cp /fastroute/support/*.dat /fastroute/
-sh /fastroute/support/ispd18_download.sh 
-sh /fastroute/support/tests/ispd18_unit_test.sh /FRlefdef
+cd fastroute/
+tclsh support/tests/run_all.tcl
