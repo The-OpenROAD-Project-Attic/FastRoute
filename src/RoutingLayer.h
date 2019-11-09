e57b14f35a6e6e6c01e8d057cc6c0ae8d476bf1d
eder_matheus <emrmonteiro@inf.ufrgs.br>
2019-11-08 21:00:42 -0300

    Add routing layer class

---
+++ src/RoutingLayer.h
@@ -0,0 +1,72 @@
+////////////////////////////////////////////////////////////////////////////////
+// Authors: Vitor Bandeira, Eder Matheus Monteiro e Isadora
+// Oliveira
+//          (Advisor: Ricardo Reis)
+//
+// BSD 3-Clause License
+//
+// Copyright (c) 2019, Federal University of Rio Grande do Sul (UFRGS)
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __ROUTINGLAYER_H_
+#define __ROUTINGLAYER_H_
+
+#include <string>
+#include <vector>
+#include <functional>
+#include <limits>
+#include <iostream>
+
+#include "Coordinate.h"
+#include "Box.h"
+#include "Pin.h"
+
+class RoutingLayer {
+private:
+        int _index;
+        std::string _name;
+        bool _preferredDirection;
+        
+public:
+        RoutingLayer() = default;
+        RoutingLayer(const int index, const std::string name,
+                     const bool preferredDirection)
+            : _index(index), _name(name),
+              _preferredDirection(preferredDirection) {}
+        
+        const static bool HORIZONTAL = 0;
+        const static bool VERTICAL = 1;
+        
+        int getIndex() const { return _index; }
+        std::string getName() const { return _name; }
+        bool getPreferredDirection() const { return _preferredDirection; }
+};
+
+#endif /* __ROUTINGLAYER_H_ */
