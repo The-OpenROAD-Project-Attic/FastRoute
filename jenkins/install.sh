949db5bd09c083e69d328c32f9633cd7ff060f55
eder_matheus <emrmonteiro@inf.ufrgs.br>
2019-11-20 11:36:05 -0300

    Update Jenkins script to avoid Tcl wrapper issue

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -1,4 +1,5 @@
 mkdir -p /fastroute/build
+rm -f /fastroute/src/TclInterfaceWrap.cpp
 cd /fastroute/build
 cmake -DCMAKE_BUILD_TYPE=Release -DBOOST_ROOT=/opt/rh/rh-mongodb32/root/usr ..  && \
     make
