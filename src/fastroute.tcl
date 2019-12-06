dadd51e35f5c0e26beec78655289956e231d1eb1
Eder Matheus Rodrigues Monteiro <emrmonteiro@inf.ufrgs.br>
2019-12-06 18:28:46 -0300

    Fix indentation

--- src/fastroute.tcl
+++ src/fastroute.tcl
@@ -2,18 +2,18 @@
 sta::define_cmd_args "run_global_routing" {[-output_file out_file]}
 
 proc run_global_routing { args } {
-    sta::parse_key_args "run_global_routing" args \
-        keys {-output_file} flags {}
+  sta::parse_key_args "run_global_routing" args \
+    keys {-output_file} flags {}
 
-    if { [info exists keys(-output_file)] } {
-        set out_file $keys(-output_file)
-        FastRoute::set_output_file $out_file
-    } else {
-        puts "WARNING: Default output guide name: out.guide"
-        FastRoute::set_output_file "out.guide"
-    }
+  if { [info exists keys(-output_file)] } {
+    set out_file $keys(-output_file)
+    FastRoute::set_output_file $out_file
+  } else {
+    puts "WARNING: Default output guide name: out.guide"
+    FastRoute::set_output_file "out.guide"
+  }
 
-    FastRoute::start_fastroute
-    FastRoute::run_fastroute
-    FastRoute::write_guides
+  FastRoute::start_fastroute
+  FastRoute::run_fastroute
+  FastRoute::write_guides
 }
