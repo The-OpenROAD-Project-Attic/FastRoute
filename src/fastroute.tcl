1e3c39a5fc32b0aba58b822a9f672529828e2e95
eder_matheus <emrmonteiro@inf.ufrgs.br>
2019-12-12 21:29:18 -0300

    Add capacity adjustments, min and max routing layer options into top level Tcl command

--- src/fastroute.tcl
+++ src/fastroute.tcl
@@ -1,9 +1,48 @@
+# ////////////////////////////////////////////////////////////////////////////////
+# // Authors: Vitor Bandeira, Eder Matheus Monteiro e Isadora Oliveira
+# //          (Advisor: Ricardo Reis)
+# //
+# // BSD 3-Clause License
+# //
+# // Copyright (c) 2019, Federal University of Rio Grande do Sul (UFRGS)
+# // All rights reserved.
+# //
+# // Redistribution and use in source and binary forms, with or without
+# // modification, are permitted provided that the following conditions are met:
+# //
+# // * Redistributions of source code must retain the above copyright notice, this
+# //   list of conditions and the following disclaimer.
+# //
+# // * Redistributions in binary form must reproduce the above copyright notice,
+# //   this list of conditions and the following disclaimer in the documentation
+# //   and/or other materials provided with the distribution.
+# //
+# // * Neither the name of the copyright holder nor the names of its
+# //   contributors may be used to endorse or promote products derived from
+# //   this software without specific prior written permission.
+# //
+# // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# // ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# // LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# // CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# // SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# // INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# // CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# // POSSIBILITY OF SUCH DAMAGE.
+# ////////////////////////////////////////////////////////////////////////////////
 
-sta::define_cmd_args "run_global_routing" {[-output_file out_file]}
+sta::define_cmd_args "run_global_routing" {[-output_file out_file] \
+                                           [-capacity_adjustment cap_adjust] \
+                                           [-min_routing_layer min_layer] \
+                                           [-max_routing_layer max_layer] \
+}
 
 proc run_global_routing { args } {
   sta::parse_key_args "run_global_routing" args \
-    keys {-output_file} flags {}
+    keys {-output_file -capacity_adjustment -min_routing_layer -max_routing_layer} flags {}
 
   if { [info exists keys(-output_file)] } {
     set out_file $keys(-output_file)
@@ -11,6 +50,30 @@ proc run_global_routing { args } {
   } else {
     puts "WARNING: Default output guide name: out.guide"
     FastRoute::set_output_file "out.guide"
+  }
+
+  if { [info exists keys(-capacity_adjustment)] } {
+    set cap_adjust $keys(-capacity_adjustment)
+    FastRoute::set_capacity_adjustment $cap_adjust
+  } else {
+    puts "WARNING: Default capacity adjustment: 0.0 (no adjustment)"
+    FastRoute::set_capacity_adjustment 0.0
+  }
+
+  if { [info exists keys(-min_routing_layer)] } {
+    set min_layer $keys(-min_routing_layer)
+    FastRoute::set_min_layer $min_layer
+  } else {
+    puts "WARNING: Default minimum layer: 1"
+    FastRoute::set_min_layer 1
+  }
+
+  if { [info exists keys(-max_routing_layer)] } {
+    set max_layer $keys(-max_routing_layer)
+    FastRoute::set_max_layer $max_layer
+  } else {
+    puts "WARNING: Default minimum layer: -1 (last layer)"
+    FastRoute::set_max_layer -1
   }
 
   FastRoute::start_fastroute
