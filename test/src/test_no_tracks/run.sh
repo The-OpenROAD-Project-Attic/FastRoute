0fc0f28f3c7c1697f8507a1cfe59ebd65bbe074c
eder_matheus <emrmonteiro@inf.ufrgs.br>
2020-04-01 00:14:27 -0300

    Add unit test for designs with missing tracks

---
+++ test/src/test_no_tracks/run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+GREEN=0
+RED=2
+
+if [ "$#" -ne 2 ]; then
+	exit 2
+fi
+
+binary=$1
+testdir=$2
+
+$binary -no_init < run.tcl > log.txt 2>&1
+ 
+grep -q "missing track structure" log.txt
+
+status=$?
+
+if [ $status -eq 0 ]; then
+    exit $GREEN
+else
+    exit $RED
+fi
